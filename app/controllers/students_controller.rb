00892094a82059f0ebfdcaa7b78dfc4e70dcc102
Arianna Kosten <arianna.kosten@gmail.com>
2019-09-07 03:23:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
   
   def create 
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to students_path #
+    redirect_to students_path 
+    #Print out the student value to the view template page
   end 
 end
